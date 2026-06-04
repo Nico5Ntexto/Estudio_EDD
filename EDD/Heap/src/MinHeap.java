1963e7e70924539322242b80100d17f48e59404d
Nico5Ntexto <nicolasignaciogonzalez0308@gmail.com>
2026-06-03 21:06:26 -0400

    Editado heap

--- EDD/Heap/src/MinHeap.java
+++ EDD/Heap/src/MinHeap.java
@@ -8,10 +8,53 @@ class MinHeap{
         size = 0;
         capacity = heap.length;
     }
+    //----------------MÉTODOS PÚBLICOS----------------
+    public void insert(int value){
+        if(isFull()) duplicateCapacity();
 
+        heap[size] = value;
+        heapifyUp(size++);
+    }
+
+    //----------------MÉTODOS PRIVADOS----------------
     private int padre(int i){return (i - 1)/2;}
     private int izquierda(int i){return 2 * i + 1;}
     private int derecha(int i){return 2 * i + 2;}
-   //probando 
+
+    private void heapifyUp(int i){
+        while(i > 0 && heap[i] < heap[padre(i)]){
+            swap(i, padre(i));
+            i = padre(i);
+        }
+    }
+
+    private void heapifyDown(int i){
+        int masChico = i;
+
+        if(izquierda(i) < size && heap[i] > heap[izquierda(i)]){
+            masChico = izquierda(i);
+        }
+
+        if(derecha(i) < size && heap[masChico] > heap[derecha(i)]){
+            masChico = derecha(i);
+        }
+
+        if(masChico != i){
+            swap(masChico, i);
+            heapifyDown(masChico);
+        }
+    }
+
+    private void swap(int i, int j){
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+    private boolean isFull(){return size == capacity;}
+    private boolean isEmpty(){return size == 0;}
+    private void duplicateCapacity(){
+        capacity *= 2;
+        heap = java.util.Arrays.copyOf(heap, capacity);
+    }
     
 }
--- EDD/heap.java
+++
@@ -1,6 +0,0 @@
-
-public class heap {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
-}
