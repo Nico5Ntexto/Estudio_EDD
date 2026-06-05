92f56712fe0f634d5a5e51834096c75a07d001a4
Nicolas Barra <166175490+NicolasBarraC@users.noreply.github.com>
2026-06-05 16:22:03 -0400

    haciendo nuevamente clase maxheap

--- EDD/Heap/src/maxheapNB.java
+++ EDD/Heap/src/maxheapNB.java
@@ -1,78 +1,39 @@
-public class maxheapNB {
-    int[] A;
+public class maxheapNB{
+    int [] A;
     int heapSize;
-
-    // Cambiamos el constructor para recibir una capacidad inicial, no un arreglo lleno.
-    public maxheapNB(int[] A) {
-        this.A = A;
-        this.heapSize = 0;
+    maxheapNB(int[] A){
+        this.A=A;
+        heapSize=0;
     }
-//probando modificacion 2
-    public int padre(int i) {
-        return (i - 1) / 2;
-    }
-
-    public void swap(int i, int j) {
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
-    }
-
-    public void insertar(int v) {
-        if (heapSize == A.length) {
-            //[1,2,3,v,x1,x2,x3] |size|
-            //size == A.length
-            A = java.util.Arrays.copyOf(A, A.length * 2);
+    public void increaseKey(int i, int v) throws Exception{
+        if(i>=A.length || i<0){
+            throw new Exception ("Indice no valido");
         }
-        //[1,2,3,size,0,0,0]
-        int i = heapSize;
         heapSize++;
-        //[1,2,3,i,size,0,0]
-        A[i] = Integer.MIN_VALUE; //0x8000_0000
-        //[1,2,3,0x8000_0000,size,0,0]
-        increaseKey(i, v);
-    }
-
-    public boolean increaseKey(int i, int v) {
-        if (A[i] > v) {
-            return false;
-        } else {
-            A[i] = v;
-            //[1,2,3,v,size,0,0]
-            // CORRECCIÓN: Comparamos el valor de i (A[i]) con el valor del padre (A[padre(i)])
-            while (i > 0 && A[i] > A[padre(i)]) {
-                swap(i, padre(i));
-                i = padre(i);
-            }
-            return true;
+        A[heapSize-1]=Integer.MIN_VALUE;
+        A[heapSize-1]=A[i];
+        if(A[i]>A[padre(i)]){
+            swap(i,padre(i));
         }
     }
-
-    public void imprimir() {
-        // CORRECCIÓN: Imprimimos hasta heapSize, no hasta la longitud máxima del arreglo
-        for (int i = 0; i < heapSize; i++) {
-            System.out.print(A[i] + " ");
+    public void swap(int i, int j){
+        int temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
+    }
+    public int padre(int i){
+        return (i-1)/2;
+    }
+    public void imprimir(){
+        for (int i=0;i<A.length;i++){
+            System.out.print(A[i]);
         }
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        // Creamos un heap con capacidad inicial de 15
-        
-        // Arreglo de prueba
-        int[] elementos = {1, 4, 52, 32, 5, 3, 4, 6, 76, 64, 23, 43, 1};
-        
-        maxheapNB h1 = new maxheapNB(elementos);
-        
-        // Insertamos los elementos correctamente uno por uno para construir el Max Heap
-        System.out.println("Insertando elementos iniciales...");
-        for (int i = 0; i < elementos.length; i++) {
-            h1.insertar(elementos[i]);
-        }
-        h1.imprimir();
-        
-        System.out.println("Insertando el número 100...");
-        h1.insertar(100);
-        h1.imprimir();
+    
+    public static void main(String[] args){
+        int[] A={1,3,6,4,2,3,1,5};
+        maxheapNB h1=new maxheapNB(A);
     }
 }
